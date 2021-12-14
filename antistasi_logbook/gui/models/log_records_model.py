db7ab46984748223e690bd2c51df5f2043995d64
Giddius <GordonGrossmann@gmx.at>
2021-12-14 13:38:19 +0100

    puh

--- antistasi_logbook/gui/models/log_records_model.py
+++ antistasi_logbook/gui/models/log_records_model.py
@@ -60,18 +60,21 @@ from PySide6 import QtCore, QtGui, QtWidgets
 from colorhash import ColorHash
 from antistasi_logbook.gui.models.base_query_data_model import BaseQueryDataModel
 from gidapptools.general_helper.string_helper import StringCaseConverter
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QRunnable, QThreadPool, Slot,
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale, QMetaObject, QObject, QPoint, QRect, QSize, QThread, QTime, QUrl, Qt, QRunnable, QThreadPool, Slot, Signal,
                             QAbstractTableModel, QAbstractItemModel, QAbstractListModel, QEvent, QModelIndex)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QGradient, QIcon, QImage, QKeySequence,
+from PySide6.QtGui import (QAction, QBrush, QFontMetrics, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QGradient, QIcon, QImage, QKeySequence,
                            QLinearGradient, QPainter, QPalette, QPixmap, QRadialGradient, QTransform, QCloseEvent)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QMenu, QMenuBar, QSizePolicy, QStatusBar, QWidget, QPushButton, QLabel, QProgressBar, QProgressDialog,
                                QBoxLayout, QHBoxLayout, QVBoxLayout, QSizePolicy, QMessageBox, QLayout, QGroupBox, QStyledItemDelegate, QAbstractItemDelegate, QDockWidget, QTabWidget, QSystemTrayIcon, QTableView, QListView, QTreeView, QColumnView)
 from antistasi_logbook.records.antistasi_records import PerformanceRecord
 from gidapptools import get_logger
+from threading import Event, Condition, Lock
 import pp
+from playhouse.shortcuts import model_to_dict
+from gidapptools.general_helper.color.color_item import Color
 if TYPE_CHECKING:
     from antistasi_logbook.backend import Backend
-
+    from antistasi_logbook.gui.views.log_records_query_table_view import LogRecordsQueryTreeView
     from antistasi_logbook.backend import Backend
     from antistasi_logbook.storage.models.models import BaseModel
     from antistasi_logbook.records.abstract_record import AbstractRecord
@@ -95,40 +98,107 @@ log = get_logger(__name__)
 # endregion[Constants]
 
 
-class LogRecordsModel(BaseQueryDataModel):
+class WorkerThread(QThread):
+    def __init__(self, func: Callable, parent=None, **kwargs):
+        QThread.__init__(self, parent)
+        self.func = func
+        self.kwargs = kwargs
 
+    def run(self):
+        self.func(**self.kwargs)
+
+
+RefreshItem = namedtuple("RefreshItem", ["idx", "item"])
+
+
+class LogRecordsModel(BaseQueryDataModel):
+    added_first_row = Signal()
+    added_multiline_item = Signal(int)
+    request_resize_to_contents = Signal(list)
     bool_images = {True: None,
                    False: None}
 
-    def __init__(self, backend: "Backend") -> None:
-        super().__init__(backend, LogRecord)
+    def __init__(self, backend: "Backend", parent=None) -> None:
+        super().__init__(backend, LogRecord, parent=parent)
         self.filter = []
-        self.ordered_by = LogRecord.recorded_at
+        self.ordered_by = (LogRecord.start, LogRecord.recorded_at)
+        self.generator_refresh_chunk_size = 25
+        self.message_column_font = self._make_message_column_font()
+
+    @property
+    def resize_lock(self) -> Lock:
+        return self.parent().resize_lock
+
+    def _make_message_column_font(self) -> QFont:
+        font = QFont()
+        font.setFamily("Cascadia Mono")
+        return font
 
     @property
     def column_names_to_exclude(self) -> set[str]:
-        return self._column_names_to_exclude.union({"log_file"})
+        return self._column_names_to_exclude.union({"log_file", "record_class", "logged_from", "called_by"})
 
     def get_query(self) -> "Query":
-        query = LogRecord.select()
-        for _join_model in [LogFile, LogLevel, RecordClass]:
-            query = query.join(_join_model).switch(LogRecord)
         logged_from_alias = AntstasiFunction.alias()
-        query = query.join(AntstasiFunction, on=LogRecord.called_by, join_type=JOIN.LEFT_OUTER).switch(LogRecord).join(logged_from_alias, on=LogRecord.logged_from, join_type=JOIN.LEFT_OUTER)
-        query = query.where(reduce(or_, self.filter))
-        return query.order_by(self.ordered_by)
+        query = LogRecord.select().join(LogFile, on=LogRecord.log_file).switch(LogRecord).join(LogLevel, on=LogRecord.log_level).switch(
+            LogRecord).join(RecordClass, on=LogRecord.record_class).switch(LogRecord).join(AntstasiFunction, on=LogRecord.called_by, join_type=JOIN.LEFT_OUTER).switch(
+            LogRecord).join(logged_from_alias, on=LogRecord.logged_from, join_type=JOIN.LEFT_OUTER).switch(LogRecord).where(reduce(or_, self.filter)).order_by(*self.ordered_by)
+        return query
 
     def _get_display_data(self, index: "INDEX_TYPE") -> Any:
         item = self.content_items[index.row()]
         column = self.columns[index.column()]
         if column.name == "message":
             return item.pretty_message
-        data = item.get_data(column.name)
+        data = getattr(item, column.name)
         if data is None:
             return self.on_display_data_none(role=Qt.DisplayRole, item=item, column=column)
         if isinstance(data, bool):
             return self.on_display_data_bool(role=Qt.DisplayRole, item=item, column=column, value=data)
         return str(data)
+
+    def _get_background_data(self, index: "INDEX_TYPE") -> Any:
+        item = self.content_items[index.row()]
+        column = self.columns[index.column()]
+        if item.log_level is None:
+            log.critical("log_level is None for %r", item)
+        elif item.log_level.name == "ERROR":
+            return Color(225, 25, 23, 0.5, "error_red").qcolor
+
+        return item.color
+
+    def _make_size(self, item: Union["AbstractRecord", "LogRecord"]) -> Union["AbstractRecord", "LogRecord"]:
+        message = item.pretty_message.strip()
+        metrics = QFontMetrics(self.message_column_font)
+        b_rect = metrics.boundingRect(message)
+        width = b_rect.width()
+        height = 0
+        for line in message.splitlines():
+            height += metrics.boundingRect(line).height()
+        item.message_size_hint = QSize(width, height)
+        return item
+
+    def _get_size_hint_data(self, index: "INDEX_TYPE") -> Any:
+        item = self.content_items[index.row()]
+        column = self.columns[index.column()]
+        if column.name == "message":
+            # if item.message_size_hint is None:
+
+            #     message = item.pretty_message.strip()
+            #     metrics = QFontMetrics(self.message_column_font)
+            #     b_rect = metrics.boundingRect(message)
+            #     width = b_rect.width()
+            #     height = 0
+            #     for line in message.splitlines():
+            #         height += metrics.boundingRect(line).height()
+            #     item.message_size_hint = QSize(width, height)
+
+            return item.message_size_hint
+
+    def _get_font_data(self, index: "INDEX_TYPE") -> Any:
+        column = self.columns[index.column()]
+        if column.name == "message":
+            return self.message_column_font
 
     def get_content(self) -> "BaseQueryDataModel":
         self.content_items = [i.to_record_class() for i in self.get_query()]
@@ -139,28 +209,96 @@ class LogRecordsModel(BaseQueryDataModel):
         self.columns = tuple(sorted(columns, key=lambda x: self.column_ordering.get(x.name.casefold(), 99)))
         return self
 
-    def refresh(self) -> None:
+    # def refresh(self, abort_signal: Event = None) -> "LogRecordsModel":
+    #     self.get_columns()
+
+    #     items = []
+
+    #     if self.content_items is None:
+    #         self.content_items = []
+    #     else:
+    #         self.content_items.clear()
+    #     self.modelReset.emit()
+    #     idx = 0
+    #     for item in self.get_query().execute():
+    #         idx += 1
+    #         record_item = item.to_record_class()
+
+    #         items.append(RefreshItem(idx, record_item))
+    #         if len(items) == self.generator_refresh_chunk_size:
+    #             request_idxs = []
+    #             self.beginInsertRows(QtCore.QModelIndex(), min(i.idx for i in items), max(i.idx for i in items))
+    #             for i in items:
+    #                 _item = i.item
+    #                 if _item.___has_multiline_message___ is True:
+    #                     request_idxs.append(i.idx)
+    #                 self.content_items.append(self._make_size(_item))
+    #             self.endInsertRows()
+    #             if 5 in {i.idx for i in items}:
+    #                 self.parent().resizeColumnsToContents()
+    #             self.request_resize_to_contents.emit(request_idxs)
+
+    #             items.clear()
+
+    #     if len(items) != 0:
+    #         self.beginInsertRows(QtCore.QModelIndex(), min(i.idx for i in items), max(i.idx for i in items))
+    #         for i in items:
+    #             _item = i.item
+    #             if _item.___has_multiline_message___ is True:
+    #                 request_idxs.append(i.idx)
+    #             self.content_items.append(self._make_size(_item))
+    #         self.endInsertRows()
+    #         self.request_resize_to_contents.emit(request_idxs)
+    #     return self, abort_signal
+
+    def _generator_refresh(self, abort_signal: Event = None) -> "LogRecordsModel":
+        def to_pri(**kwargs):
+            pp(dict(kwargs))
         self.layoutAboutToBeChanged.emit()
         self.get_columns()
+
+        items = []
+
         if self.content_items is None:
             self.content_items = []
         else:
             self.content_items.clear()
         self.layoutChanged.emit()
+        idx = 0
+        with self.backend.database:
+            for item in self.get_query().iterator():
+                _ = model_to_dict(item, exclude=[LogRecord.log_file])
+                idx += 1
+                record_item = item.to_record_class()
+                items.append(RefreshItem(idx, record_item))
+                if len(items) == self.generator_refresh_chunk_size:
+                    self.beginInsertRows(QtCore.QModelIndex(), min(i.idx for i in items), max(i.idx for i in items))
 
-        self.layoutAboutToBeChanged.emit()
+                    self.content_items += [self._make_size(i.item) for i in items]
 
-        for idx, item in enumerate(self.get_query().execute()):
-            record_item = item.to_record_class()
+                    self.endInsertRows()
+                    while len(items) > 0:
+                        item = items.pop().item
+                        _ = item.log_level
+                        _ = item.record_class
+                        _ = item.called_by
+                        _ = item.logged_from
+                    items.clear()
+                    sleep(1)
 
-            self.content_items.append(record_item)
-            if idx % 250 == 0:
-                self.layoutChanged.emit()
+        if len(items) != 0:
+            self.beginInsertRows(QtCore.QModelIndex(), min(i.idx for i in items), max(i.idx for i in items))
 
-        self.modelReset.emit()
+            self.content_items += [i.item for i in items]
+            self.endInsertRows()
+            items.clear()
+        self.parent().resizeColumnToContents([i for i, col in enumerate(self.columns) if col.name == "message"][0])
+        self.layoutChanged.emit()
+        return self, abort_signal
 
-    def generator_refresh(self):
-        return self.refresh()
+    def generator_refresh(self, abort_signal: Event = None) -> tuple["BaseQueryDataModel", Event]:
+        instance = WorkerThread(self._generator_refresh, abort_signal=abort_signal, parent=self)
+        instance.start()
 
 
 # region[Main_Exec]
--- antistasi_logbook/records/base_record.py
+++ antistasi_logbook/records/base_record.py
@@ -53,6 +53,14 @@ from importlib.machinery import SourceFileLoader
 
 from antistasi_logbook.records.abstract_record import AbstractRecord, RecordFamily, MessageFormat
 from antistasi_logbook.records.enums import LogLevelEnum, PunishmentActionEnum
+from gidapptools.general_helper.color.color_item import RGBColor, Color
+
+try:
+    from PySide6.QtGui import QColor, QFont, QFontMetrics
+    PYSIDE6_AVAILABLE = True
+except ImportError:
+    PYSIDE6_AVAILABLE = False
+
 if TYPE_CHECKING:
     from antistasi_logbook.storage.models.models import LogFile, LogRecord, PunishmentAction, LogLevel, AntstasiFunction
     from antistasi_logbook.parsing.parser import RawRecord
@@ -78,12 +86,44 @@ THIS_FILE_DIR = Path(__file__).parent.absolute()
 class BaseRecord(AbstractRecord):
     ___record_family___ = RecordFamily.GENERIC | RecordFamily.ANTISTASI
     ___specificity___ = 0
-    __slots__ = ("log_record",)
+
+    __slots__ = ("log_record", "message_size_hint", "_color")
 
     def __init__(self,
-                 log_record: "LogRecord"
-                 ) -> None:
+                 log_record: "LogRecord") -> None:
         self.log_record = log_record
+        self.message_size_hint = None
+        self._color: RGBColor = None
+
+    @property
+    def color(self) -> Optional[RGBColor]:
+        if self._color is None:
+            self._color = Color.get_color_by_name("LightGray").with_alpha(0.5).qcolor
+        return self._color
+
+    @cached_property
+    def start(self) -> int:
+        return self.log_record.start
+
+    @cached_property
+    def end(self) -> int:
+        return self.log_record.end
+
+    @cached_property
+    def log_level(self):
+        return self.log_record.log_level
+
+    @cached_property
+    def called_by(self):
+        return self.log_record.called_by
+
+    @cached_property
+    def logged_from(self):
+        return self.log_record.logged_from
+
+    @cached_property
+    def log_file(self):
+        return self.log_record.log_file
 
     def mark(self) -> None:
         self.log_record.marked = True
@@ -105,9 +145,13 @@ class BaseRecord(AbstractRecord):
 
     def __getattr__(self, name: str) -> Any:
         try:
-            return self.log_record.get_data(name)
+            _out = self.log_record.get_data(name)
+
         except (KeyError, AttributeError):
-            return getattr(self.log_record, name)
+            _out = None
+        if _out is None:
+            _out = getattr(self.log_record, name)
+        return _out
 
     @classmethod
     def check(cls, raw_record: "RawRecord") -> bool:
